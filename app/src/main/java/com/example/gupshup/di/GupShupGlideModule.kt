984e515fa5506d9bb2c0f84cf3fcf53fba3596ce
MUHAMMAD SAAD <saadw7751@gmail.com>
2026-02-17 23:08:13 +0500

    some minor changes

---
+++ app/src/main/java/com/example/gupshup/di/GupShupGlideModule.kt
@@ -0,0 +1,7 @@
+package com.example.gupshup.di
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GupShupGlideModule : AppGlideModule()
